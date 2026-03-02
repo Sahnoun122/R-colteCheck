7f6431261a10e8023fbc90844c4e34127827d02b
sahnoun122 <khadijasahnoun70@gmail.com>
2026-03-02 14:37:22 +0000

    feat: changer la structure de serivce pour l'authentifications

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,12 +3,31 @@ import {
   signInWithEmailAndPassword,
   sendPasswordResetEmail,
   signOut,
+  updateProfile,
 } from "firebase/auth";
-
 import { auth } from "../firebase/firebase.config";
+import { createMyProfile } from "./profile.service";
 
-export async function register(email: string, password: string) {
+export async function register(
+  email: string,
+  password: string,
+  fullName: string,
+) {
   const cred = await createUserWithEmailAndPassword(auth, email, password);
+
+  await updateProfile(cred.user, { displayName: fullName });
+
+  await createMyProfile({
+    uid: cred.user.uid,
+    fullName,
+    email: cred.user.email ?? email,
+    phone: "",
+    city: "",
+    farmName: "",
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+  });
+
   return cred.user;
 }
 
