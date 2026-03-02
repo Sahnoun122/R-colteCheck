11a18767863e9a264eb2535c71488cd20d1be99d
sahnoun122 <khadijasahnoun70@gmail.com>
2026-03-02 14:11:50 +0000

    feat: ajouter le type de l'agriculteur

---
+++ src/types/agriculteur.ts
@@ -0,0 +1,10 @@
+export type AgriculteurProfile = {
+  uid: string;
+  fullName: string;
+  email: string;
+  phone?: string;
+  city?: string;
+  farmName?: string;
+  createdAt: number;
+  updatedAt: number;
+};
