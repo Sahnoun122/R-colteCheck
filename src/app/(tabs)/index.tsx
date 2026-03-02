a0c8a31676331ff3f7e90181bf6366736aa0c856
sahnoun122 <khadijasahnoun70@gmail.com>
2026-03-02 12:04:02 +0000

    feat:ajouter la page index.ts

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,45 @@
-import { View, Text } from "react-native";
-
+import React from "react";
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import { router } from "expo-router";
+import { useAuth } from "@/src/context/AuthContext";
+import { logout } from "@/src/services/auth.service";
 export default function HomeTab() {
+  const { user } = useAuth();
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Tableau de bord</Text>
+      <Text style={styles.subtitle}>
+        Bienvenue sur votre espace agriculteur
+      </Text>
+
+      <Text style={styles.card}>
+        Connecté en tant que : {user?.email ?? "—"}
+      </Text>
+
+      <Pressable
+        style={styles.button}
+        onPress={async () => {
+          await logout();
+          router.replace("/(auth)/login");
+        }}
+      >
+        <Text style={styles.buttonText}>Se déconnecter</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 20, justifyContent: "center", gap: 12 },
+  title: { fontSize: 26, fontWeight: "700" },
+  subtitle: { fontSize: 14, opacity: 0.7 },
+  card: { padding: 12, borderWidth: 1, borderColor: "#ddd", borderRadius: 10 },
+  button: {
+    backgroundColor: "#111",
+    padding: 14,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonText: { color: "#fff", fontSize: 16, fontWeight: "600" },
+});
