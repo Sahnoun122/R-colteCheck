8a2e54c4c8a0b1d3581d66d2ee68bea9e7283855
sahnoun122 <khadijasahnoun70@gmail.com>
2026-03-05 15:42:53 +0000

    feat: regler le probleme de route

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,31 +1,10 @@
-import { useEffect } from "react";
-import { Stack, useRouter, useSegments } from "expo-router";
-import { AuthProvider, useAuth } from "@/src/context/AuthContext";
-
-function RootLayoutNav() {
-  const { user, loading } = useAuth();
-  const router = useRouter();
-  const segments = useSegments();
-
-  useEffect(() => {
-    if (loading) return;
-
-    const inAuthGroup = segments[0] === "(auth)";
-
-    if (!user && !inAuthGroup) {
-      router.replace("/(auth)/login");
-    } else if (user && inAuthGroup) {
-      router.replace("/(tabs)");
-    }
-  }, [user, loading, segments]);
-
-  return <Stack screenOptions={{ headerShown: false }} />;
-}
+import { AuthProvider } from "@/src/context/AuthContext";
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <RootLayoutNav />
+      <Stack screenOptions={{ headerShown: false }} />
     </AuthProvider>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,17 @@
+import { useAuth } from "@/src/context/AuthContext";
+import { Redirect } from "expo-router";
 import { ActivityIndicator, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <ActivityIndicator size="large" color="#111" />
-    </View>
-  );
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#111" />
+      </View>
+    );
+  }
+
+  return <Redirect href={user ? "/(tabs)" : "/(auth)/login"} />;
 }
